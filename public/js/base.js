12157cdd52cbb02705e9d1ec06c1cbba71da06de
Renan <renanandreolla@outlook.com>
2022-03-13 18:08:18 -0300

    Product - category - PDP - cart

--- public/js/base.js
+++ public/js/base.js
@@ -2,7 +2,7 @@ toastr.options = {
     "closeButton": false,
     "debug": false,
     "newestOnTop": false,
-    "progressBar": false,
+    "progressBar": true,
     "positionClass": "toast-top-center",
     "preventDuplicates": false,
     "onclick": null,
@@ -15,3 +15,17 @@ toastr.options = {
     "showMethod": "fadeIn",
     "hideMethod": "fadeOut"
 };
+
+function showCartItems () {
+    var cart = sessionStorage.getItem("cart");
+    var quantity = 0;
+    if (cart !== null) {
+        cart = JSON.parse(cart);
+        quantity = cart.length;
+    }
+    $('#cart-items').html(quantity);
+}
+
+$(document).ready(function () {
+    showCartItems();
+});
