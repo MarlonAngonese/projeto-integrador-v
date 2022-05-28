3f726b2395f7bdef8a3282ca1e1299b81a2a60cd
MarlonAngonese <marlonmaster2020@gmail.com>
2022-05-28 04:53:19 -0300

    Organize html files in groups

--- src/front-routes/routes.js
+++ src/front-routes/routes.js
@@ -15,7 +15,7 @@ const Products = mongoose.model('products', ProductsSchema);
 // GET CATEGORY
 router.get('/admin/categories', (req, res) => {
     Categories.find((err, obj) => {
-        res.render('insertCategory.html', { categories: obj });
+        res.render('categories/add.category.html', { categories: obj });
     });
 });
 
@@ -23,26 +23,26 @@ router.get('/admin/categories', (req, res) => {
 router.get('/admin/insertProducts', (req, res) => {
     Products.find((err, products) => {
         Categories.find().sort('name').exec((err, categories) => {
-            res.render('insertProducts.html', { products: products, categories: categories });
+            res.render('products/add.products.html', { products: products, categories: categories });
         });
     });
 });
 
-router.get('/admin/c/:slug', (req, res) => {
-    Categories.aggregate([
-        { $match: { slug: req.params.slug } },
-        {
-            $lookup: {
-                from: "products", // collection name in db
-                localField: "_id",
-                foreignField: "category",
-                as: "products"
-            }
-        }]).exec((err, obj) => {
-            console.info(obj);
-            res.render('products.html', { products: obj[0].products });
-        });
-});
+// router.get('/admin/c/:slug', (req, res) => {
+//     Categories.aggregate([
+//         { $match: { slug: req.params.slug } },
+//         {
+//             $lookup: {
+//                 from: "products", // collection name in db
+//                 localField: "_id",
+//                 foreignField: "category",
+//                 as: "products"
+//             }
+//         }]).exec((err, obj) => {
+//             console.info(obj);
+//             res.render('products/list.products.html', { products: obj[0].products });
+//         });
+// });
 
 // SEARCH PRODUCTS
 router.get('/admin/search', (req, res) => {
@@ -54,7 +54,7 @@ router.get('/admin/search', (req, res) => {
         queryObj = { "name": { "$regex": query, "$options": "i" } };
     }
     Products.find(queryObj).sort([cond]).exec((err, products) => {
-        res.render('products.html', { products: products, q: query });
+        res.render('products/list.products.html', { products: products, q: query });
     });
 });
 
@@ -64,40 +64,40 @@ router.get('/', (req, res) => {
 });
 
 router.get('/register', (req, res) => {
-    res.render('register.html');
+    res.render('clients/register.html');
 });
 
 router.get('/contact', (req, res) => {
-    res.render('contact.html');
+    res.render('clients/contact.html');
 });
 
 router.get('/products', (req, res) => {
-    res.render('products.html');
+    res.render('products/list.products.html');
 });
 
 router.get('/product', (req, res) => {
-    res.render('product.html');
+    res.render('products/view.product.html');
 });
 
 router.get('/cart', (req, res) => {
-    res.render('cart.html');
+    res.render('clients/cart.html');
 });
 
 router.get('/checkout', (req, res) => {
     console.log(req.session);
-    res.render('checkout.html', { client: req.session.client });
+    res.render('clients/checkout.html', { client: req.session.client });
 });
 
 router.get('/login', (req, res) => {
-    res.render('login.html');
+    res.render('clients/login.html');
 });
 
-router.get('/payment', (req, res) => {
-    res.render('payment.html');
-});
+// router.get('/payment', (req, res) => {
+//     res.render('clients/payment.html');
+// });
 
 router.get('/confirmation', (req, res) => {
-    res.render('confirmation.html');
+    res.render('clients/confirmation.html');
 });
 
 router.get('/product/:id', (req, res) => {
@@ -106,19 +106,18 @@ router.get('/product/:id', (req, res) => {
             // podemos botar uma pagína de 404 aqui
         } else {
             const product = obj[0];
-            res.render('product.html', { product: product });
+            res.render('products/view.product.html', { product: product });
         }
     });
 });
 
 router.get('/products', (req, res) => {
     Products.find((err, obj) => {
-        res.render('products.html', { products: obj });
+        res.render('products/list.products.html', { products: obj });
     });
 });
 
 // APIS ROUTES
-
 router.get('/api/product/:id', (req, res) => {
     Products.find({ "_id": req.params.id }, (err, obj) => {
         if (err) {
